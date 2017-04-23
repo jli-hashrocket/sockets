4561c0c6982047a96c1cda50fc484a44ac69597f
macheteshuffle <jeff.li029@gmail.com>
2017-04-23 17:37:10 -0400

    manipulating timestamps with moment.js

---
+++ moment-example.js
@@ -0,0 +1,16 @@
+var moment = require('moment');
+var now = moment();
+
+// console.log(now.format());
+// console.log(now.format('X'));
+// console.log(now.valueOf());
+
+var timestamp = 1492982694772;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mma'));
+
+// now.subtract(1, 'year');
+// console.log(now.format());
+// console.log(now.format('MMM Do, YYYY h:mma'));
+
